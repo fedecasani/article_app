80eb6e49d55160a23742115e205a720e313b7b14
Federico Casani <fedecasani@gmail.com>
2024-08-13 11:09:21 -0300

    imports made

--- lib/data/repositories/article_repository_impl.dart
+++ lib/data/repositories/article_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_article_app/domain/entities/article.dart';
+import 'package:flutter_article_app/domain/repositories/article_repository.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
