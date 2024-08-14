c3df7adfe4ef3eefb5d092a313a6e2a92cbc87e9
Federico Casani <fedecasani@gmail.com>
2024-08-13 23:34:54 -0300

    improved details page

--- lib/presentation/pages/article_detail_page.dart
+++ lib/presentation/pages/article_detail_page.dart
@@ -22,38 +22,62 @@ class ArticleDetailPage extends StatelessWidget {
           },
         ),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Imagen aleatoria del artículo
-            Container(
-              height: 200, // Ajusta el tamaño según sea necesario
-              width: double.infinity,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                    'https://source.unsplash.com/random/800x600', // URL de una imagen aleatoria
+      body: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Imagen aleatoria del artículo
+                Container(
+                  height: 200, // Ajusta el tamaño según sea necesario
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        'https://picsum.photos/800/600?random=${DateTime.now().millisecondsSinceEpoch}', // URL de una imagen aleatoria
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
                   ),
-                  fit: BoxFit.cover,
                 ),
-                borderRadius: BorderRadius.circular(8),
+                SizedBox(height: 16.0), // Espacio entre la imagen y el texto
+                // Descripción del artículo
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Text(
+                      article.body,
+                      style: TextStyle(
+                          fontSize: 16), // Ajusta el estilo según sea necesario
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 16.0,
+            right: 16.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.blue[800], // Fondo azul
+                shape: BoxShape.circle, // Forma circular
+              ),
+              child: IconButton(
+                icon: Icon(
+                  Icons.edit,
+                  color: Colors.white, // Ícono blanco
+                  size: 30,
+                ),
+                onPressed: () {
+                  // Acción para el icono de editar (actualmente no hace nada)
+                },
               ),
             ),
-            SizedBox(height: 16.0), // Espacio entre la imagen y el texto
-            // Descripción del artículo
-            Expanded(
-              child: SingleChildScrollView(
-                child: Text(
-                  article.body,
-                  style: TextStyle(
-                      fontSize: 16), // Ajusta el estilo según sea necesario
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
